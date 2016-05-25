e7e23e07e97b3e50e609dbad05b501f4763fd043
John Franey <johnfraney@gmail.com>
2016-05-25 00:05:05 -0300

    Refactor app.js
    Create generic function to make calls to Kodi's HTTP API

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@
  */
 
 var UI = require('ui');
-var Vector2 = require('vector2');
 var ajax = require('ajax');
 
 // Config
@@ -15,8 +14,9 @@ var clayConfig = require('./config');
 var clay = new Clay(clayConfig, null, {autoHandleEvents: false});
 
 var kodi_url = 'http://' + Settings.option('kodi_url') + '/jsonrpc';
-console.log(kodi_url);
 
+
+// UI
 var main = new UI.Card({
   title: 'PlayPause',
   icon: 'images/menu_icon.png',
@@ -30,91 +30,63 @@ var main = new UI.Card({
 
 main.show();
 
-main.on('click', 'up', function(e) {
-  ajax(
-  {
-    url: kodi_url,
-    method: 'post',
-    type: 'json',
-    data: {"jsonrpc": "2.0", "method": "Input.Up", "id": 1}
-  },
-  function(data, status, request) {
-    console.log('Message: ' + data);
-  },
-  function(error, status, request) {
-    console.log('The ajax request failed: ' + error);
+
+// Functionality
+function send_kodi_post(command) {
+  // Set up common Kodi HTTP POST data
+  var post_data = {
+    "jsonrpc": "2.0",
+    "id": 1,
+    "method": command
+  };
+  
+  // Add POST data.params if needed by the method
+  if (command == 'Player.PlayPause') {
+    post_data.params = { "playerid": 0 };
   }
-);
+    
+  // Make AJAX POST to kodi_url
+  ajax(
+    {
+      url: kodi_url,
+      method: 'post',
+      type: 'json',
+      data: post_data
+    },
+    function(data, status, request) {
+      console.log('Message: ' + JSON.stringify(data));
+    },
+    function(error, status, request) {
+      console.log('The ajax request failed: ' + JSON.stringify(error));
+    }
+  );
+}
+
+// Up button clicks
+main.on('click', 'up', function(e) {
+  send_kodi_post("Input.Up");
 });
 
+// Select button clicks
 main.on('click', 'select', function(e) {
-  ajax(
-  {
-    url: kodi_url,
-    method: 'post',
-    type: 'json',
-    data: {"jsonrpc": "2.0", "method": "Input.Select", "id": 1}
-  },
-  function(data, status, request) {
-    console.log('Message: ' + data);
-  },
-  function(error, status, request) {
-    console.log('The ajax request failed: ' + error);
-  }
-);
+  send_kodi_post("Input.Select");
 });
 
 main.on('longClick', 'select', function(e) {
-  ajax(
-  {
-    url: kodi_url,
-    method: 'post',
-    type: 'json',
-    data: {"jsonrpc": "2.0", "method": "Player.PlayPause", "params": { "playerid": 0 }, "id": 1}
-  },
-  function(data, status, request) {
-    console.log('Message: ' + data);
-  },
-  function(error, status, request) {
-    console.log('The ajax request failed: ' + error);
-  }
-);
+  send_kodi_post("Player.PlayPause");
 });
 
+// Down button clicks
 main.on('click', 'down', function(e) {
-  ajax(
-  {
-    url: kodi_url,
-    method: 'post',
-    type: 'json',
-    data: {"jsonrpc": "2.0", "method": "Input.Down", "id": 1}
-  },
-  function(data, status, request) {
-    console.log('Message: ' + data);
-  },
-  function(error, status, request) {
-    console.log('The ajax request failed: ' + error);
-  }
-);
+  send_kodi_post("Input.Down");
 });
 
 main.on('longClick', 'down', function(e) {
-  ajax(
-  {
-    url: kodi_url,
-    method: 'post',
-    type: 'json',
-    data: {"jsonrpc": "2.0", "method": "Input.Back", "id": 1}
-  },
-  function(data, status, request) {
-    console.log('Message: ' + data);
-  },
-  function(error, status, request) {
-    console.log('The ajax request failed: ' + error);
-  }
-);
+  send_kodi_post("Input.Back");
 });
 
+
+// Config events
 Pebble.addEventListener('showConfiguration', function(e) {
   Pebble.openURL(clay.generateUrl());
 });
